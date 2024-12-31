4f0116bf0e5316ae95b602f3a824482bebc7c8cc
DanCo9715 <danielcoronado1415@gmail.com>
2024-12-31 13:05:31 -0500

    interestellar

---
+++ Intelestelar.py
@@ -0,0 +1,36 @@
+import pygame
+
+pygame.init()
+
+peso = input('Ingresa tu peso: ')
+
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption('Imagen Rotando')
+
+imagen = pygame.image.load('C:/Users/USER/desktop/BlackHole.jpeg')
+# Use one load statement only
+imagen = pygame.transform.scale(imagen, (400, 400))
+rect = imagen.get_rect(center=(300, 200))
+
+angulo = 0
+reloj = pygame.time.Clock()
+
+pygame.mixer.music.load('interstellar.mp3')
+pygame.mixer.music.play(-1)
+
+running = True
+while running:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            running = False  # Exit the loop
+
+    angulo += 1
+    imagen_rotada = pygame.transform.rotate(imagen, angulo)
+    rect_rotado = imagen_rotada.get_rect(center=rect.center)
+
+    screen.fill((255, 255, 255))
+    screen.blit(imagen_rotada, rect_rotado.topleft)
+    pygame.display.flip()
+    reloj.tick(60)  # Limit frame rate to 60 FPS
+
+pygame.quit()
